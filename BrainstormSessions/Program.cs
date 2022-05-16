7dd2074b915436d01a91527aff850917c712e6b4
Jakhongir7 <bakhodirovjakhongir@gmail.com>
2022-05-17 00:01:03 +0500

    Task 2

--- BrainstormSessions/Program.cs
+++ BrainstormSessions/Program.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.Email;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
 
 namespace BrainstormSessions
 {
@@ -9,10 +15,26 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
+            var emailLogsSmtpInfo = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText("emailLogsSmtpInfo.json"));
+
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.File("Logs\\logs.log")
+                .WriteTo.Email(new EmailConnectionInfo
+                {
+                    FromEmail = emailLogsSmtpInfo["usernameEmail"],
+                    ToEmail = emailLogsSmtpInfo["toEmail"],
+                    MailServer = emailLogsSmtpInfo["emailServer"],
+                    NetworkCredentials = new NetworkCredential
+                    {
+                        UserName = emailLogsSmtpInfo["username"],
+                        Password = emailLogsSmtpInfo["password"]
+                    },
+                    EnableSsl = true,
+                    Port = Int32.Parse(emailLogsSmtpInfo["port"]),
+                    EmailSubject = emailLogsSmtpInfo["subject"]
+                })
                 .CreateLogger();
 
             Log.Information("Starting web host");
